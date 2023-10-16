ab65fe5cddc1bcd832c836b313f8d0a98842ef6a
FelipeVieira4 <felipevieira58238@gmail.com>
2023-10-15 23:55:07 -0300

    atualizacao

--- App.java
+++ App.java
@@ -13,10 +13,15 @@ public class App
 {
     public static void main( String[] args )
     {
+        
+
+
         System.out.println( "Hello World!" );
         Cliente c1 = new Cliente(0, "Felipe V.");
 
+        
 
+        //Criar conta bancaria
         ContaBancaria cb1 = new ContaFactory().criarBancaria();
         cb1.addCliente(c1);
         cb1.addCliente(new Cliente(1, "José"));
@@ -24,17 +29,27 @@ public class App
 
         System.out.println(cb1.getId());
 
+
+        //Criar investimentos
         Investimento investimento1 = new Investimento(0,TipoInvestimeto.Agricultura,1200);
         Investimento investimento2 = new Investimento(1,TipoInvestimeto.Agricultura,600);
         
+        System.out.println(cb1.getClass().getName());
+
+
+        //Criar conta investimento
         ContaInvestimento ci1 = new ContaFactory().criarInvestimento();
         
         ci1.addInvestimento(investimento1);
         ci1.addInvestimento(investimento2);
 
+        System.out.println(ci1.getClass().getName());
+
+
         ci1.addCliente(c1);
         ci1.addCliente(new Cliente(1, "José"));
 
         System.out.println(ci1.calcularLucroMensal());
+
     }
 }

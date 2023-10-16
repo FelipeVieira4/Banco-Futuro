1db43d5107a44695c9c515b4313cddfc8515f1ec
FelipeVieira4 <felipevieira58238@gmail.com>
2023-10-15 23:26:32 -0300

    alterado array para hash

--- contas/contaBancarias/ContaInvestimento.java
+++ contas/contaBancarias/ContaInvestimento.java
@@ -1,15 +1,18 @@
 package contas.contaBancarias;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 import tipos.Investimento;
 
 public class ContaInvestimento extends ContaBancaria{
   
-  private ArrayList<Investimento> investimentos;
+  private HashMap<Integer,Investimento> listaInvestimentos = new HashMap<Integer,Investimento>();
 
-  public String helloWorld(){
-    return "h1";
+  public void addInvestimento(Investimento i){
+    listaInvestimentos.put(i.getId(), i);
   }
 
+  public void removeInvestimento(int id){
+    listaInvestimentos.remove(id);
+  }
 }
--- tipos/Investimento.java
+++ tipos/Investimento.java
@@ -1,5 +1,21 @@
 package tipos;
 
+
 public class Investimento {
-  private TipoInvestimeto tipo;  
+  private int id;
+  private TipoInvestimeto tipo;
+  private float lucroMensal; 
+
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public TipoInvestimeto getTipo() {
+    return tipo;
+  }
+  public void setTipo(TipoInvestimeto tipo) {
+    this.tipo = tipo;
+  }
 }

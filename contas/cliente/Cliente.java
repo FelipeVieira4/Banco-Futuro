64dc36d35e6fa7b4ecd26eedc5d9a1a2763d4269
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-10-18 20:29:36 -0300

    feat: adicionado mais variaveis a Cliente.java

--- contas/cliente/Cliente.java
+++ contas/cliente/Cliente.java
@@ -1,9 +1,13 @@
 package contas.cliente;
 
+import java.util.ArrayList;
+
 public class Cliente{
     private int id;
     private String nome;
     private String telefone;
+    private String cpf;
+    private ArrayList<String> enderecos = new ArrayList<String>();
 
     public Cliente(int id,String nome){
         this.setId(id);
@@ -41,6 +45,21 @@ public class Cliente{
         this.telefone = telefone;
     }
 
-    
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
+	
+	public ArrayList<String> getEnderecos() {
+		return enderecos;
+	}
+	public void setEnderecos(ArrayList<String> enderecos) {
+		this.enderecos = enderecos;
+	}
+	public void addEndereco(String endereco) {
+		this.enderecos.add(endereco);
+	}
 }

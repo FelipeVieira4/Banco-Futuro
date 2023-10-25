694960eea04667d95d0fb96c6a4a29d3afdfada9
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-10-25 19:07:38 -0300

    alterado "mensagem" para "mesangemErro"

--- contas/contaBancarias/OperacoesBancarias.java
+++ contas/contaBancarias/OperacoesBancarias.java
@@ -13,19 +13,19 @@ import tipos.Transacao;
 
 public abstract class OperacoesBancarias {
 	
-  private final String mensagem = "Classe sem supporte a esse método";
+  private final String mensagemErro = "Classe sem supporte a esse método";
 
 
   //Métodos da ContaCorrente
-  public ArrayList<Transacao> getlistaTransacoes(){throw new UnsupportedOperationException(mensagem);}
+  public ArrayList<Transacao> getlistaTransacoes(){throw new UnsupportedOperationException(mensagemErro);}
     
   //Métodos da ContaPoupança
-  public float getTaxaJuros() {throw new UnsupportedOperationException(mensagem);}
-  public void setTaxaJuros(float taxaJuros) {throw new UnsupportedOperationException(mensagem);}
-  public float calcularRendimento() {throw new UnsupportedOperationException(mensagem);}
+  public float getTaxaJuros() {throw new UnsupportedOperationException(mensagemErro);}
+  public void setTaxaJuros(float taxaJuros) {throw new UnsupportedOperationException(mensagemErro);}
+  public float calcularRendimento() {throw new UnsupportedOperationException(mensagemErro);}
 
   //Métodos da ContaInvestimento
-  public void addInvestimento(Investimento i){throw new UnsupportedOperationException(mensagem);}
-  public void removeInvestimento(String id){throw new UnsupportedOperationException(mensagem);}
-  public void calcularLucroMensal(){throw new UnsupportedOperationException(mensagem);}
+  public void addInvestimento(Investimento i){throw new UnsupportedOperationException(mensagemErro);}
+  public void removeInvestimento(String id){throw new UnsupportedOperationException(mensagemErro);}
+  public void calcularLucroMensal(){throw new UnsupportedOperationException(mensagemErro);}
 }

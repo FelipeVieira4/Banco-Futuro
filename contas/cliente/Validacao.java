e7545ebe637ed6df89fdffc9eac717dbfcc79cc8
FelipeVieira4 <felipevieira58238@gmail.com>
2023-10-28 23:20:51 -0300

    alterado local das Strings

--- contas/cliente/Validacao.java
+++ contas/cliente/Validacao.java
@@ -4,17 +4,19 @@ import java.util.regex.Pattern;
 
 public class Validacao {
 	
-	public static Boolean Email(String email) {
-		String regexEmail = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-		Pattern emailPattern = Pattern.compile(regexEmail, Pattern.CASE_INSENSITIVE);
+	private final static String regexEmail = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	private final static String regexCpf="^(\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}|\\d{11})$";
 
+
+	public static Boolean Email(String email) {
+		
+		Pattern emailPattern = Pattern.compile(regexEmail, Pattern.CASE_INSENSITIVE);
 		return emailPattern.matcher(email).find();
 	}
 	
 	public static Boolean Cpf(String cpf) {
-		String regexCpf="^(\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}|\\d{11})$";
+		
 		Pattern cpfPattern = Pattern.compile(regexCpf);
-
 		return cpfPattern.matcher(cpf).find();
 	}
 }

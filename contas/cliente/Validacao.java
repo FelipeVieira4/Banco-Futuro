2f8d8e6fa3e60368be7476ae4bb2df72d3a1a2aa
FelipeVieira4 <felipevieira58238@gmail.com>
2023-10-28 23:18:33 -0300

    Removido Matcher

--- contas/cliente/Validacao.java
+++ contas/cliente/Validacao.java
@@ -1,6 +1,5 @@
 package contas.cliente;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validacao {
@@ -9,15 +8,13 @@ public class Validacao {
 		String regexEmail = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 		Pattern emailPattern = Pattern.compile(regexEmail, Pattern.CASE_INSENSITIVE);
 
-		Matcher validar = emailPattern.matcher(email);
-		return validar.find();
+		return emailPattern.matcher(email).find();
 	}
 	
 	public static Boolean Cpf(String cpf) {
 		String regexCpf="^(\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}|\\d{11})$";
 		Pattern cpfPattern = Pattern.compile(regexCpf);
 
-		Matcher validar = cpfPattern.matcher(cpf);
-		return validar.find();
+		return cpfPattern.matcher(cpf).find();
 	}
 }
